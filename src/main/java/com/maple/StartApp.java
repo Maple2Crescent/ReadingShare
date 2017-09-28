56316d96ac109b42142316b1c1ba9c709904eea7
Maple2Crescent <547031437@qq.com>
2017-09-29 01:20:00 +0800

    spring boot + Thymeleaf model

--- src/main/java/com/maple/StartApp.java
+++ src/main/java/com/maple/StartApp.java
@@ -1,11 +1,16 @@
 package com.maple;
 
+import com.maple.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableAutoConfiguration  //1开启自动配置
 @ComponentScan  //2开启bean扫描
@@ -14,8 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class StartApp {
 
     @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    public String index(Model model) {
+        User single = new User("1","test1");
+        List<User> people = new ArrayList<>();
+        User p1 = new User("2", "test2");
+        User p2 = new User("3", "test3");
+        User p3 = new User("4", "test4");
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        model.addAttribute("singlePerson", single);
+        model.addAttribute("people", people);
+        return "index";
     }
 
     public static void main(String[] args) {
--- src/main/java/com/maple/model/User.java
+++ src/main/java/com/maple/model/User.java
@@ -8,6 +8,16 @@ public class User {
     private String userId;
     private String userName;
 
+    public User() {
+        super();
+    }
+
+    public User(String userId, String userName) {
+        super();
+        this.userId = userId;
+        this.userName = userName;
+    }
+
     public String getUserName() {
         return userName;
     }
